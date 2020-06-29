e19c7429e310c4debe508259e3ed28a6d60b81e3
ubuntu18.04 <ste.penazzi1987@gmail.com>
2020-06-28 23:53:43 -0700

    ConnectionDataPostgre using Properties file

--- src/main/java/data/source/internal/database/postgreSQL/ConnectionDataPostgre.java
+++ src/main/java/data/source/internal/database/postgreSQL/ConnectionDataPostgre.java
@@ -3,6 +3,17 @@
  */
 package data.source.internal.database.postgreSQL;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.lang.System.Logger.Level;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+
 import data.source.internal.database.ConnectionData;
 
 /**
@@ -16,6 +27,7 @@ public class ConnectionDataPostgre implements ConnectionData {
 	private String user;
 	private String password;
 	private String dataBase;
+	private String url;
 	
 	public ConnectionDataPostgre(String user,String password,String ip,String port,String database) {
 		this.ip = ip;
@@ -23,7 +35,34 @@ public class ConnectionDataPostgre implements ConnectionData {
 		this.user = user;
 		this.password = password;
 		this.dataBase = database;
+		this.url = "jdbc:postgresql://" +this.ip+":"+this.port +"/"+ this.dataBase;
 	}
+	
+	public ConnectionDataPostgre(String path) {
+		
+		Properties props = new Properties();
+        Path myPath = Paths.get("path");
+
+        try {
+            BufferedReader bf = Files.newBufferedReader(myPath, 
+                StandardCharsets.UTF_8);
+
+            props.load(bf);
+            
+            this.ip = "only in url";
+    		this.port = "only in url";
+    		this.user = props.getProperty("db.user");;
+    		this.password = props.getProperty("db.password");;
+    		this.dataBase = "only in url";
+    		this.url = props.getProperty("db.url");;
+            
+            
+        } catch (IOException ex) {
+        	Logger lgr = Logger.getLogger(PreparedStatementPostgre.class.getName());
+            lgr.log(java.util.logging.Level.SEVERE, ex.getMessage(), ex);
+        }
+	}
+	
 	
 	public String getIp() {
 		return this.ip;
@@ -41,8 +80,7 @@ public class ConnectionDataPostgre implements ConnectionData {
 		return this.dataBase;
 	}
 	public String getUrl() {
-		String s ="jdbc:postgresql://" +this.ip+":"+this.port +"/"+ this.dataBase;
-		return s;
+		return this.url;
 	}
 	
 }
