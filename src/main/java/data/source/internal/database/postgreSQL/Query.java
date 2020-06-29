24fc9d7800fae21694ec2ccdae072889eecbf6cb
ubuntu18.04 <ste.penazzi1987@gmail.com>
2020-06-29 00:45:02 -0700

    Query postgre

---
+++ src/main/java/data/source/internal/database/postgreSQL/Query.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package data.source.internal.database.postgreSQL;
+
+/**
+ * @author stefanopenazzi
+ *
+ */
+public class Query {
+	
+	public static void insertFromCsv(ConnectionDataPostgre connectiondata,String table,String path) {
+		
+	}
+	
+	public static void insertFromJson(ConnectionDataPostgre connectiondata,String table,String path) {
+		
+	}
+
+}
